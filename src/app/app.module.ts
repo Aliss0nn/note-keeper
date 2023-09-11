c5c53b701ec12e8beebe0bce80db67bc19e69563
Alisson  Scopel <alli_scopel@hotmail.com>
2023-09-11 19:31:23 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardNotaComponent } from './components/notas/card-nota/card-nota.component';
+import { ListarNotasComponent } from './components/notas/listar-notas/listar-notas.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardNotaComponent,
+    ListarNotasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/notas/listar-notas/listar-notas.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Nota } from '../Nota';
+
+@Component({
+  selector: 'app-listar-notas',
+  templateUrl: './listar-notas.component.html',
+  styleUrls: ['./listar-notas.component.css']
+})
+export class ListarNotasComponent {
+
+  notas: Nota[] = [
+    {
+      id: 0,
+      titulo: 'Lavar o cachorro 🦮',
+      conteudo: 'Pegar a toalha > Pegar o Shampoo',
+      tema: 'dark',
+    },
+    {
+      id: 1,
+      titulo: 'Prepara Aula',
+      conteudo: 'Prerparar Jamboard',
+      tema: 'warning',
+    },
+    {
+      id: 2,
+      titulo: 'AaAAAADAAAAAAAAAAAAAAAa',
+      conteudo: 'Testando os cards',
+      tema: 'danger',
+    },
+  ];
+
+}
